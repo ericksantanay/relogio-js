b65ab29f41428c4b91929e806a4940b55ca5b00c
Erick Silva <donizttierick@gmail.com>
2025-12-06 15:45:01 -0300

    .

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// Chamando as saídas 
+let h = document.getElementById('h')
+let m = document.getElementById('m')
+let s = document.getElementById('s')
+
+// SetInterval
+setInterval(function(){
+    // Data Atual
+    let data = new Date()
+
+    // Hora
+    
+
+}, 1000)
