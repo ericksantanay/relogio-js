866c2045a6057a3c5521018afb9dbf8013bcf8c0
Erick Silva <donizttierick@gmail.com>
2025-12-06 15:52:26 -0300

    Script Finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,14 @@ setInterval(function(){
     let data = new Date()
 
     // Hora
-    
+    let hora = String(data.getHours()).padStart(2, '0')
+    h.innerText = (hora)
 
+    // Minutos
+    let minutos = String(data.getMinutes()).padStart(2, '0')
+    m.innerText = (minutos)
+
+    // Segundos
+    let segundos = String(data.getSeconds()).padStart(2, '0') 
+    s.innerText = (segundos)
 }, 1000)
